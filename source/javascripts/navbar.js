338d3b37f915a8c020b721b31e2ce55eab30d1a8
Yin Lee <leesrrh@gmail.com>
2023-07-16 18:10:15 +0900

    fixed navbar hiding content problem

---
+++ source/javascripts/navbar.js
@@ -0,0 +1,8 @@
+// window.addEventListener("scroll", function() {
+//   var navbar = document.getElementById("try");
+//   if (window.pageYOffset > 80) {
+//     navbar.classList.add("transparent-navbar");
+//   } else {
+//     navbar.classList.remove("transparent-navbar");
+//   }
+// });
